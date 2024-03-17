80ed9c25a352b922c39d25a358258793dc927999
souhila boukari <suhamimisou14@gmail.com>
2024-03-17 14:12:26 -0400

    practicing node

---
+++ app.js
@@ -0,0 +1,13 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs)
+
+// const animalArray = ["dog", "cat", "horse"];
+// animalArray.push("cow");
+
+
+// const personObj = {
+//     name: "souhila",
+//     age: 28
+// }
+// personObj.age = 29;
+// personObj.occupation = "Developer";
