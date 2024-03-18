f26512783320d463795cd568005c2e907676aab5
souhila boukari <suhamimisou14@gmail.com>
2024-03-18 16:42:34 -0400

    ininitial set up

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
-
-// const animalArray = ["dog", "cat", "horse"];
-// animalArray.push("cow");
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
 
-// const personObj = {
-//     name: "souhila",
-//     age: 28
-// }
-// personObj.age = 29;
-// personObj.occupation = "Developer";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const printProfileData = profileDataArr => {
+
+    // this ...
+    for(let i = 0; i < profileDataArr.length; i+= 1){
+        console.log(profileDataArr[i]);
+    }
+
+
+
+
+
+ console.log("=======");
+
+
+ // is that same as this ....
+ profileDataArr.forEach((profileItem) => {
+    console.log(profileItem);
+ });
+
+   
+};
+printProfileData();
