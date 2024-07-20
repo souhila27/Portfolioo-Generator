e05c362be979111a6da4e49ef2baf1ced0229063
souhila boukari <suhamimisou14@gmail.com>
2024-07-20 12:44:36 -0400

    adding new file

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+// const printProfileData = profileDataArr => {
 
 
+//     // is that same as this ....
+//     profileDataArr.forEach((profileItem) => {
+//         console.log(profileItem);
+//     });
 
 
+// };
+// printProfileData(profileDataArgs);
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+}
+fs.writeFile('index.html', generatePage(name, github), err =>{
+    if(err) throw err;
 
 
-
-
-
-
-
-
-const printProfileData = profileDataArr => {
-
-
-    // is that same as this ....
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem);
-    });
-
-
-};
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! checkout index.htnl to see the output!');
+});
+ 
