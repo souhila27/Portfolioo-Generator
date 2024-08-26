e1ebfe53c5b281bdb7d2cde2a77a7bf540df701f
Souhila Boukari <suhamimisou14@gmail.com>
2024-08-25 22:50:06 -0400

    inquirere issue fixed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const inquirer = require('inquirer');
+import inquirer from 'inquirer';
 // const fs = require('fs');
 // const generatePage = require("./src/page-template.js");
 // const pageHTML = generatePage(name, gitHub);
