b6e67be2c7aeb9f0950c9acfcad34133745e870f
Souhila Boukari <souhila_boukari@shi.com>
2025-09-02 17:10:45 -0400

    continue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 import inquirer from 'inquirer';
-import fs from 'fs';
 import generatePage from './src/page-template.js';
+import { writeFile, copyFile } from './utils/generate-site.js';
 
 
 const promptUser = () => {
@@ -127,12 +127,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./dist/index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
